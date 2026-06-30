e3843df5696d35bfc99897af58c4728903a7cbd7
JethroAmaga <jethroargabnr@gmail.com>
2026-06-30 14:33:59 +0700

    Add files via upload

--- api/chat.js
+++ api/chat.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     }
   }
 
-  const MODEL = "meta/llama-3.1-8b-instruct";
+  const MODEL = "meta/llama-3.3-70b-instruct";
 
   const nimMessages = system ? [{ role: "system", content: system }, ...messages] : messages;
 
